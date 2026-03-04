b618cb19add418312d5dc120a762d039ef62ba92
deborahohannedev <deborahohanne@gmail.com>
2026-03-04 16:39:09 -0300

    Update seed.py

--- seed.py
+++ seed.py
@@ -21,6 +21,13 @@ def seed_data(db: Session):
         ("⏰", "Atrasado(a)", -1),
         ("😴", "Dorminhoco(a)", -1),
         ("💸", "Gastador(a)", -1),
+        ("😶", "Fala demais", -1),
+        ("🥱", "Chato(a)", -2),
+        ("🏃‍♂️", "Forinha", -1),
+        ("🥳", "Inimigo do fim", 2),
+        ("🪴", "Planta", -2),
+        ("👥", "Boa companhia", 2),
+        ("💣", "Fora da viagem", -3),
     ]
 
     for icon, name, points in emojis:
